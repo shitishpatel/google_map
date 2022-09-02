66c424f9e929354ba4c1809677c7fac527667916
shitish_patel <shitishpatel1999@gmail.com>
2022-09-02 18:04:16 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+// import { MouseEvent } from '@agm/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'googlemap2';
+export class AppComponent  {
+  lat:any;
+  lng:any;
+  map(item:any){
+    this.lat= item.lat;
+    this.lng = item.lng;
+  }
 }
